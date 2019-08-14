20fc03742d2a66a51d04c1f6a017c13053bcaa63
Mikeal Rogers <mikeal.rogers@gmail.com>
2019-08-14 00:47:27 +0000

    debug: adding some debug statements

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,13 +8,17 @@ fi
 
 # initialize git
 remote_repo="https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
-git init
+echo "test1"
 git config user.name "${GITHUB_ACTOR}"
 git config user.email "${GITHUB_ACTOR}@users.noreply.github.com"
 git remote add publish-origin "${remote_repo}"
-
+git show-ref # useful for debugging
+echo "test2"
 # publish any new files
 git add -A
 timestamp=$(date -u)
+echo "test3"
 git commit -m "Automated publish: ${timestamp} ${GITHUB_SHA}"
+echo "test4"
 git push publish-origin master
+echo "test5"
