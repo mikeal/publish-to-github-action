3b76ab76b496e10a7d646d062c916f833fe7e690
Luigi Di Fraia <luigidifraia@users.noreply.github.com>
2021-04-29 00:19:40 +0100

    Quote the commit message variable

--- entrypoint.sh
+++ entrypoint.sh
@@ -30,6 +30,6 @@ git lfs install
 # publish any new files
 git checkout ${BRANCH_NAME}
 git add -A
-git commit -m ${COMMIT_MESSAGE} || exit 0
+git commit -m "${COMMIT_MESSAGE}" || exit 0
 git pull --rebase publisher ${BRANCH_NAME}
 git push publisher ${BRANCH_NAME}
